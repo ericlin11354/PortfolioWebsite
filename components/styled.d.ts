8450b14cc53365ab08dd195396fe1e04206eb978
ericlin11354 <ericlin11354@gmail.com>
2021-12-20 19:55:02 -0500

    Created Global component for custom theming system

--- components/styled.d.ts
+++ components/styled.d.ts
@@ -1,7 +1,7 @@
 import 'styled-components';
-import { MainTheme, MainThemeInterface } from './Themes';
+import { MainThemeInterface } from './Themes';
 
 // Add theme to 'styled-components'
 declare module 'styled-components' {
-    export interface DefaultTheme extends MainThemeInterface { }
+    export interface DefaultTheme extends MainThemeInterface {}
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,18 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { NavBar } from "../components/Containers/NavBar";
+import { Global, ThemeTypes } from "../components/Containers";
 
 const Home: NextPage = () => {
   return (
-  <div>
-    <div>Hello World!</div>
-    <NavBar height={"100px"}/>
-  </div>
+    <Global
+      theme={ThemeTypes.MAIN}
+    >
+      <div>
+        <div>Hello World!</div>
+        <NavBar height={"100px"}/>
+      </div>
+    </Global>
   );
 };
 
